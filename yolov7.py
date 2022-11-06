10927a6c13327c25d83ed60ced51bb25da152b07
Blane_C <bcypurda286@gmail.com>
2022-11-06 12:56:21 -0600

    giving up on this

--- yolov7.py
+++ yolov7.py
@@ -32,23 +32,27 @@ pipeline = dai.Pipeline()
 
 # Define sources and outputs
 camRgb = pipeline.create(dai.node.ColorCamera)
-spatialDetectionNetwork = pipeline.create(dai.node.MobileNetSpatialDetectionNetwork)
-
 detectionNetwork = pipeline.create(dai.node.YoloDetectionNetwork)
 xoutRgb = pipeline.create(dai.node.XLinkOut)
+xoutVideo = pipeline.create(dai.node.XLinkOut)
 nnOut = pipeline.create(dai.node.XLinkOut)
 
+xoutVideo.setStreamName("video")
 xoutRgb.setStreamName("rgb")
 nnOut.setStreamName("nn")
 
 
 # Properties
-camRgb.setPreviewSize(640, 640)
-camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
 camRgb.setInterleaved(False)
+camRgb.setVideoSize(640, 640)
+camRgb.setPreviewSize(1920, 1080)
 camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
 camRgb.setFps(40)
 
+xoutVideo.input.setBlocking(False)
+xoutVideo.input.setQueueSize(1)
 
 # Network specific settings
 detectionNetwork.setConfidenceThreshold(model_dict['nn_config']['NN_specific_metadata']['confidence_threshold'])
@@ -64,6 +68,7 @@ detectionNetwork.input.setBlocking(False)
 labelMap = model_dict['mappings']['labels']
 
 # Linking
+camRgb.video.link(xoutVideo.input)
 camRgb.preview.link(detectionNetwork.input)
 if syncNN:
     detectionNetwork.passthrough.link(xoutRgb.input)
@@ -78,6 +83,7 @@ with dai.Device(pipeline) as device:
     # Output queues will be used to get the rgb frames and nn data from the outputs defined above
     qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
     qDet = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
+    video = device.getOutputQueue(name="video", maxSize=1, blocking=False)
 
     frame = None
     detections = []
@@ -105,12 +111,16 @@ with dai.Device(pipeline) as device:
         if syncNN:
             inRgb = qRgb.get()
             inDet = qDet.get()
+            videoIn = video.get()
         else:
             inRgb = qRgb.tryGet()
             inDet = qDet.tryGet()
 
         if inRgb is not None:
-            frame = inRgb.getCvFrame()
+            frameRGB = inRgb.getCVFrame()
+
+        if videoIn is not None:
+            frame = videoIn.getCvFrame()
             cv2.putText(frame, "NN fps: {:.2f}".format(counter / (time.monotonic() - startTime)),
                         (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color2)
 
@@ -119,6 +129,9 @@ with dai.Device(pipeline) as device:
             counter += 1
 
         if frame is not None:
+            displayFrame("video", frame)
+
+        if frameRGB is not None:
             displayFrame("rgb", frame)
 
         if cv2.waitKey(1) == ord('q'):
